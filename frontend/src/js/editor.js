13ff3a0328710223da62cdcb9b452d9841d70fdf
Jussi Mitteli <jussi.mitteli@gmail.com>
2026-04-04 21:12:01 +0300

    Block controls started, move ups and down done

--- frontend/src/js/editor.js
+++ frontend/src/js/editor.js
@@ -16,3 +16,29 @@ const renderEditor = () => {
     });
 };
 
+//Creates DOM element for single block based on its type
+const createBlockElement = (block, index) => {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('block-wrapper');
+
+    //Block controls (move up/down, delete)
+    const controls = document.createElement('div');
+    controls.classList.add('block-controls');
+
+    //Move up button, moves the block up in tree
+    const moveUpBtn = document.createElement('button');
+    moveUpBtn.classList.add('btn-icon');
+    moveUpBtn.title = 'Move up';
+    moveUpBtn.textContent = '↑';
+    moveUpBtn.addEventListener('click', () => moveBlock(index, -1));
+
+    //Move down button, moves a block down in tree
+    const moveDownBtn = document.createElement('button');
+    moveDownBtn.classList.add('btn-icon');
+    moveDownBtn.title = 'Move Down';
+    moveDownBtn.textContent = '↓';
+    moveDownBtn.addEventListener('click', () => moveBlock(index, 1));
+
+
+
+}
